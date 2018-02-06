7afc051b64fe60dd57faee226ac6215daaff8d80
Harshit Singh <harshitks2203@gmail.com>
2018-02-07 00:20:16 +0530

    inital commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # React-Youtube
+
+Its a Youtube Dummy version based on React which will render your search query and shows top 5 search results.
+
+This is just for learning purpose. 
---
+++ src/index.js
@@ -0,0 +1,49 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideDetail from './components/video_detail';
+const API_KEY = 'AIzaSyBG-iFiTAxVWpdq1YiMkFf1_UVKTARnnIU';
+
+class App extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+        this.videoSearch('surfboards');
+    }
+    
+    videoSearch(term) {
+        
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({ 
+                videos: videos,
+                selectedVideo: videos[0] 
+            });
+        });
+
+    }
+
+    render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+        return (
+            <div>
+                <SearchBar onSearchTermChange={videoSearch} />
+                <div className="row">
+                    <VideoList 
+                    onVideoSelect={selectedVideo => this.setState({selectedVideo}) }
+                    videos = {this.state.videos} /> 
+                    <VideDetail video={this.state.selectedVideo} />
+                </div>
+            </div>
+            );
+        }
+    }
+
+ReactDOM.render(<App />, document.querySelector('.container'));
